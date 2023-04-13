76b2a1ae833116f202c7745e54702ed7bf1e89a5
ocean_tako <ryotaro615@gmail.com>
2023-04-13 14:48:47 +0900

    setting_se

--- tetrisweb/settings.py
+++ tetrisweb/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'nv!7_l-(+jje_-c46($va$i3g=28i^8t(%sek_w3^#3j91o1ma'
 from .settings_local import *
 
 # # SECURITY WARNING: don't run with debug turned on in production!
